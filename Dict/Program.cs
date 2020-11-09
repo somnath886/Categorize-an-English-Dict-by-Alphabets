7de8f7b495106a9a82af450783bf5f215a49e08e
somnathh886 <somnathh886@gmail.com>
2020-11-09 19:10:51 +0530

    Add project files.

---
+++ Dict/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"../../../large.txt");
+
+            char first = 'a';
+
+            string path = $"../../../DictbyAlpha(do not delete this folder)/{first}.txt";
+
+            List<string> wordList = new List<string>();
+
+            foreach (string line in lines)
+            {
+                if (line[0] == first)
+                {
+                    wordList.Add(line);
+                }
+
+                else if (line[0] != first)
+                {
+                    File.WriteAllLines(path, wordList.ToArray());
+
+                    first = line[0];
+
+                    path = $"../../../DictbyAlpha(do not delete this folder)/{first}.txt";
+
+                    wordList.Clear();
+
+                    wordList.Add(line);
+                }
+            }
+
+            File.WriteAllLines(path, wordList.ToArray());
+        }
+    }
+}
